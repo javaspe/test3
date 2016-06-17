5ad578e6f8d10b882fc4d90d5c1e9d8a51316d9c
javaspe <java@nigne.net>
2016-06-17 12:44:48 +0900

    테스트

---
+++ Hiber/src/main/java/com/study/hiber/HomeController.java
@@ -0,0 +1,41 @@
+package com.study.hiber;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.study.hiber.domain.EmpVO;
+import com.study.hiber.persistence.EmpDAO;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@Inject
+	private EmpDAO dao;
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		
+		List<EmpVO> list = dao.getList();
+		
+		model.addAttribute("l", list );
+		
+		return "home";
+	}
+	
+}
